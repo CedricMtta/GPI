d78cf783b6cf0fadeeb2c2c360c2ac009bfc63bc
CedricMtta <cmoschetta@hotmail.fr>
2018-01-30 18:39:55 +0100

    Modif *.sql

--- src/main/resources/db/migration/V1_1__image.sql
+++ src/main/resources/db/migration/V1_1__image.sql
@@ -1,5 +1,5 @@
 INSERT INTO image 
-VALUES (1, '/chemin/test1.png', 'test1.png','Quentin', '2010-04-05' );
+VALUES (1, '/chemin/test1.png', 'test1.png', 10, '2010-04-05' );
 
 INSERT INTO image 
-VALUES (2, '/chemin/test2.png', 'test2.png','Cedric', '2018-01-25' );
+VALUES (2, '/chemin/test2.png', 'test2.png', 11, '2018-01-25' );
