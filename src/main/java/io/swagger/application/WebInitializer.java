ed535bf4d55887f949ce0125ed41fa9140996428
cindyklr <cindy.kessler@hotmail.fr>
2018-02-01 20:30:50 +0100

    Modification pour déploiement

---
+++ src/main/java/io/swagger/application/WebInitializer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.swagger.application;
+
+/**
+ *
+ * @author ck
+ */
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+public class WebInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+            return application.sources(Application.class);
+    }
+}
