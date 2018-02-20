df4165de9a11e09a367e779c959aa1583145f4a0
CedricMtta <cmoschetta@hotmail.fr>
2018-02-20 21:44:01 +0100

    clean

--- src/main/java/io/swagger/api/ImageApi.java
+++ src/main/java/io/swagger/api/ImageApi.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/io/swagger/api/ImageApiController.java
+++ src/main/java/io/swagger/api/ImageApiController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.hibernate.Hibernate;
 
 import io.swagger.annotations.ApiParam;
 import io.swagger.model.Image;
