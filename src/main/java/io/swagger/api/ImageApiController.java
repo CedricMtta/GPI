562d7d5652d7a3c9aaa0466a2200c566b9a498ae
FournierNico <nico-fournier@hotmail.fr>
2018-03-07 11:19:29 +0100

    Finish JavaDoc

--- src/main/java/io/swagger/api/ImageApiController.java
+++ src/main/java/io/swagger/api/ImageApiController.java
@@ -1,7 +1,6 @@
 package io.swagger.api;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.validation.Valid;
 
@@ -20,18 +19,34 @@ import io.swagger.model.Image;
 import io.swagger.repository.ImageRepository;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-01-16T20:34:37.063Z")
 
+/**
+ * Implementation of methods from ImageApi.
+ * Describe behavior of requests.
+ *
+ */
 @RestController
 public class ImageApiController implements ImageApi {
 	
+	/**
+	 * Repository for image
+	 */
 	@Autowired
 	private ImageRepository repo;
 
+	/**
+	 * Post an image in the database
+	 * @return image with status OK
+	 */
     public ResponseEntity<Image> createImageUsingPOST(@ApiParam(value = "image" ,required=true )  @Valid @RequestBody Image image) {
     	image.setId(null);
     	image = repo.save(image);
         return new ResponseEntity<Image>(image, HttpStatus.OK);
     }
 
+    /**
+     * Delete an image from database with his id
+     * @return OK if image exist, NOT_FOUD if not
+     */
     public ResponseEntity<Void> deleteImageUsingDELETE(@ApiParam(value = "id",required=true ) @PathVariable("id") Long id) {
     	if(repo.exists(id))
     	{
@@ -42,11 +57,19 @@ public class ImageApiController implements ImageApi {
     		return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * Get a page with images.
+     * @return a page with images and status OK 
+     */
     public ResponseEntity<Page<Image>> getAllImagesUsingGET(Pageable pageable) {
     	Page<Image> images = repo.findAll(pageable);   	
         return new ResponseEntity<Page<Image>>(images, HttpStatus.OK);
     }
 
+    /**
+     * Get a page by his id.
+     * @return the image with status OK if found, status NOT_FOUND otherwise
+     */
     public ResponseEntity<Image> getImageUsingGET(@ApiParam(value = "id",required=true ) @PathVariable("id") Long id) {
     	Image image = new Image();
     	image = repo.findOne(id);
@@ -56,6 +79,10 @@ public class ImageApiController implements ImageApi {
     		return new ResponseEntity<Image>(image,HttpStatus.OK);
     }
 
+    /**
+     * Update an image using his id.
+     * @return the image modified with status OK if found, status NOT_FOUND otherwise
+     */
     public ResponseEntity<Image> updateImageUsingPUT(@ApiParam(value = "id",required=true ) @PathVariable("id") Long id, @Valid @RequestBody Image image) {
     	Image img = repo.findOne(id);
     	if(img == null)
@@ -68,6 +95,9 @@ public class ImageApiController implements ImageApi {
     	}
     }
 
+    /**
+     * Upload
+     */
 	@Override
 	public ResponseEntity<Void> uploadImage(Long id, MultipartFile file) {
 		Image img = repo.findOne(id);
@@ -86,6 +116,11 @@ public class ImageApiController implements ImageApi {
 			return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
 	}
 
+	/**
+	 * Get only the picture of an image using his id.
+	 * @param id the id of the image
+	 * @return a tab of bytes representing the picture with status OK if found, status NOT_FOUND otherwise
+	 */
 	@Override
 	public ResponseEntity<byte[]> downloadImage(Long id) {
 		Image img = repo.findOne(id);
@@ -95,6 +130,12 @@ public class ImageApiController implements ImageApi {
 			return new ResponseEntity<byte[]>(HttpStatus.NOT_FOUND);
 	}
 
+	/**
+	 * Get a page of images from an user
+	 * @param idUtil the id of the user
+	 * @param pageable the page for images
+	 * @return a page with images from the user with status OK
+	 */
 	@Override
 	public ResponseEntity<Page<Image>> findByUser(Long idUtil, Pageable pageable) {
 		return new ResponseEntity<Page<Image>>(repo.findByIdUtilisateur(idUtil, pageable), HttpStatus.OK);
